424b350346e5cc671d0f2127240c9c9474b5b916
Xiao Gui <xgui3783@gmail.com>
2023-06-23 20:16:37 +0200

    init

---
+++ main.py
@@ -0,0 +1,134 @@
+import click
+import subprocess
+from pathlib import Path
+from tempfile import mkdtemp
+import os
+from time import sleep
+import signal
+import shutil
+
+NAME_SPACE="siibra-violet"
+
+def get_module_path(arg, git_url):
+    if Path(arg).is_dir():
+        module_path = arg
+        used_tmp = False
+    else:
+        module_path = mkdtemp()
+        subprocess.run(["git", "clone", "-b", arg, git_url, module_path])
+        used_tmp = True
+    def callback():
+        if not used_tmp:
+            return
+        shutil.rmtree(module_path, ignore_errors=True)
+    return module_path, callback
+
+@click.command()
+@click.option("--config", help="Path to local config, or commit'ish on main repo. Default to latest release.")
+@click.option("--spy", help="Path to local siibra-python repo, or version tag, or commit'ish on main repo. Default to latest release.")
+@click.option("--sapi", help="Path to local siibra-api, or version tag, or commit'ish on main repo. Default to latest release.")
+@click.option("--sxplr", help="Path to local siibra-explorer, or version tag, or commit'ish on main repo. Default to latest release.")
+@click.option("--clean", help="Clean install", default=False)
+def main(config, spy, sapi, sxplr, clean):
+    
+    cleanup_callbacks = []
+
+    def cleanup():
+        # in the event that more items gets appended to cleanup_callback while it is been iterated over.
+        while len(cleanup_callbacks) > 0:
+            cb = cleanup_callbacks.pop()
+            try:
+                cb()
+            except Exception as e:
+                print("callback called unsuccessfully")
+                print(e)
+
+    config_path, config_callback = get_module_path(config, "https://jugit.fz-juelich.de/t.dickscheid/brainscapes-configurations.git")
+    cleanup_callbacks.append(config_callback)
+    
+    spy_path, spy_callback = get_module_path(spy, "https://github.com/FZJ-INM1-BDA/siibra-python.git")
+    cleanup_callbacks.append(spy_callback)
+
+    sapi_path, sapi_callback = get_module_path(sapi, "https://github.com/fzj-inm1-bda/siibra-api.git")
+    cleanup_callbacks.append(sapi_callback)
+
+    sxplr_path, sxplr_callback = get_module_path(sxplr, "https://github.com/fzj-inm1-bda/siibra-explorer.git")
+    cleanup_callbacks.append(sxplr_callback)
+
+    def log(filename):
+        fp = open(filename, "a")
+        cleanup_callbacks.append(lambda: fp.close())
+        return fp
+    
+    try:
+        if clean:
+            print("clean flag set, uninstall siibra")
+            subprocess.run(["pip", "uninstall", "-y", "siibra"], stdout=log(f"{NAME_SPACE}-siibra-python.log"), stderr=subprocess.STDOUT)
+        
+        print("installing siibra-python")
+        subprocess.run(["pip", "install", "-e", spy_path], stdout=log(f"{NAME_SPACE}-siibra-python.log"), stderr=subprocess.STDOUT)
+        print("installing siibra-api")
+        subprocess.run(["pip", "install", "-r", Path(sapi_path) / "requirements" / "all.txt" ], stdout=log(f"{NAME_SPACE}-siibra-api.log"), stderr=subprocess.STDOUT)
+
+        print("pulling redis")
+        subprocess.run(["docker", "pull", "redis"], stdout=log(f"{NAME_SPACE}-redis-pull.log"), stderr=subprocess.STDOUT)
+
+        print("starting redis container")
+        subprocess.Popen(["docker", "run",
+                        "-it",
+                        "-p", "127.0.0.1:6379:6379",
+                        "--rm",
+                        "--name", f"{NAME_SPACE}-redis",
+                        "redis"], stdout=log(f"{NAME_SPACE}-redis.log"), stderr=subprocess.STDOUT)
+        cleanup_callbacks.append(lambda: subprocess.run([ "docker", "stop", f"{NAME_SPACE}-redis" ], stdout=log(f"{NAME_SPACE}-redis.log"), stderr=subprocess.STDOUT))
+
+        api_process = subprocess.Popen(["uvicorn", "api.server:api", "--port", "10081"], cwd=sapi_path, start_new_session=True, env={
+            **os.environ,
+            "SIIBRA_USE_CONFIGURATION": config_path,
+        }, stdout=log(f"{NAME_SPACE}-siibra-api.log"), stderr=subprocess.STDOUT)
+        cleanup_callbacks.append(lambda: api_process.send_signal(signal.SIGTERM))
+
+        def process_sxplr_env(line: str):
+            return line.lstrip().lstrip("/") if "endpoint-local-10081" in line else f"// {line}"
+        
+        path_to_env = Path(sxplr_path, "src/environments/environment.common.ts")
+        with open(path_to_env, "r") as fp:
+            original_lines = fp.readlines()
+            new_lines = [
+                process_sxplr_env(line) if "endpoint" in line else line
+                for line in original_lines
+            ]
+        with open(path_to_env, "w") as fp:
+            fp.write("".join(new_lines))
+
+        def spxlr_env_cleanup():
+            with open(path_to_env, "w") as fp:
+                fp.write("".join(original_lines))
+        cleanup_callbacks.append(spxlr_env_cleanup)
+            
+        print("installing siibra-explorer dependencies")
+        subprocess.run(["npm", "i"], cwd=sxplr_path, stdout=log(f"{NAME_SPACE}-siibra-explorer-install.log"), stderr=subprocess.STDOUT)
+
+        print("running dev siibra-explorer")
+        sxplr_process = subprocess.Popen(["./node_modules/.bin/ng", "serve"], cwd=sxplr_path, start_new_session=True, stdout=log(f"{NAME_SPACE}-siibra-explorer.log"), stderr=subprocess.STDOUT)
+        def kill_npm_process():
+            sxplr_process.send_signal(signal.SIGTERM)
+        cleanup_callbacks.append(kill_npm_process)
+    except:
+        cleanup()
+        raise
+
+    sleep(10)
+    print("siibra-explorer should be ready on http://localhost:8080/")
+    
+    while True:
+        try:
+            sleep(5)
+        except KeyboardInterrupt:
+            print("keyboardinterrupt")
+            cleanup()
+            break
+
+
+if __name__ == "__main__":
+    main()
